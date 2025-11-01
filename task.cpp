aa3938c287e7132e1bad8e77c17d57f67e649132
xRonnyn <pashuha.k@gmail.com>
2025-11-01 20:31:32 +0300

    make mtx

---
+++ task.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int **convert(const int *t, size_t n, const size_t *lns, size_t rows);
+int **make(int rows, int cols);
+int main() {
+  int t[] = {5, 5, 5, 5, 6, 6, 7, 7, 7, 7, 7, 8};
+  size_t lns[] = {4, 2, 5, 1};
+  size_t rows = 4;
+  size_t n = 12;
+}
+
+int **make(int rows, int cols) {
+  int **mtx = new int *[rows];
+  for (size_t i = 0; i < rows; i++) {
+    try {
+      mtx[i] = new int[cols];
+    } catch (const std::bad_alloc &) {
+      rm(mtx, rows);
+      throw;
+    }
+  }
+  return mtx;
+}
+
+int **converts(const int *t, size_t n, const size_t *lns, size_t rows) {}
