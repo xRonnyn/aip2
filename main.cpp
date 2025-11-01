498171eebe288e0763f401f363c58d47d4fc30dc
xRonnyn <pashuha.k@gmail.com>
2025-11-01 12:39:01 +0300

    rm matrix

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 
 int **make(int rows, int cols);
 void output(const int *const *mtx);
-void rm(int **mtx);
+void rm(int **mtx, int rows);
 
 int main() {
   int rows = 0, cols = 0;
@@ -14,4 +14,10 @@ int main() {
   mtx = make(rows, cols);
   output(mtx);
   rm(mtx);
+}
+void rm(int **mtx, int rows) {
+  for (size_t i = 0; i < rows; ++i) {
+    delete[] mtx[i];
+  }
+  delete[] mtx;
 }
