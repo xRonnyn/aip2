1f885bfe5b1ee245d7f7e6792e5569aa20325da0
xRonnyn <pashuha.k@gmail.com>
2025-11-01 12:33:04 +0300

    base functions

--- main.cpp
+++ main.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
 
+int **make(int rows, int cols);
+void output(const int *const *mtx);
+void rm(int **mtx);
+
+int main() {
+  int rows = 0, cols = 0;
+  std::cin >> rows >> cols;
+  if (std::cin.fail()) {
+    return 1;
+  }
+  int **mtx = nullptr;
+  mtx = make(rows, cols);
+  output(mtx);
+  rm(mtx);
+}
